fdfa9fd5d34849c173b342ee22f174f185b2309d
Developer2 <Developer2>
2022-07-19 17:28:49 +0900

    chats page

---
+++ pages/chats.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+
+const Chats: NextPage = () => {
+  return (
+    <div className="py-10 px-4 divide-y-[1px]">
+      {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+        <div key={i} className="flex py-3 space-x-3 ">
+          <div className="bg-slate-300 rounded-full h-10 w-10" />
+          <div className="flex flex-col justify-start">
+            <p className="font-bold text-gray-900 cursor-pointer">Steve Jebs</p>
+            <p className="text-sm text-gray-600">See you tomorrow</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Chats;
