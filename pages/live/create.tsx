6e9c69f90e8c26335e08d55eb4bc2eb68c5438df
windowPC <drivespublic@gmail.com>
2022-07-21 23:19:01 +0900

    live creater and upload refactoring

--- pages/live/create.tsx
+++ pages/live/create.tsx
@@ -1,29 +1,9 @@
-import type { NextPage } from "next";
+import { NextPage } from "next";
 
-const Upload: NextPage = () => {
+const LiveCreater: NextPage = () => {
   return (
-    <div className="py-14 px-4 space-y-4">
-      <div>
-        <label className="flex justify-center items-center cursor-pointer w-full h-56 border-2 border-dashed rounded-md border-gray-400 text-gray-500 hover:border-orange-400 hover:text-orange-400">
-          <svg
-            className="h-12 w-12"
-            stroke="currentColor"
-            fill="none"
-            viewBox="0 0 48 48"
-            aria-hidden="true"
-          >
-            <path
-              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-              strokeWidth={2}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-
-          <input className="hidden" type="file" />
-        </label>
-      </div>
-      <div className="flex flex-col justify-start">
+    <div className="my-10 mx-4 space-y-5">
+      <div className="mt-3 flex flex-col justify-start">
         <label htmlFor="name" className="text-sm font-medium">
           Name
         </label>
@@ -34,7 +14,7 @@ const Upload: NextPage = () => {
           placeholder="홍길동"
         />
       </div>
-      <div className="flex flex-col justify-start">
+      <div className="mt-3 flex flex-col justify-start">
         <label className="text-sm font-medium">Price</label>
         <div className="flex relative mt-1">
           <div className="absolute flex justify-center items-center left-3 h-full pointer-events-none text-sm text-gray-500">
@@ -50,18 +30,18 @@ const Upload: NextPage = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col justify-start">
+      <div className="mt-3 flex flex-col justify-start">
         <label className="text-sm font-medium">Description</label>
         <textarea
           className="mt-1 appearance-none w-full px-3 border-transparent border-gray-300 shadow-sm placeholder-gray-400 rounded-md focus:outline-none focus:ring-orange-500 focus:border-orange-500"
           rows={4}
         />
       </div>
-      <button className="w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-bold focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
-        Upload product
+      <button className="mt-5 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-bold focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
+        Go Live
       </button>
     </div>
   );
 };
 
-export default Upload;
+export default LiveCreater;
