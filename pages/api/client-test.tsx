69884ef9690785f655df151e36d80a25e19f4489
windowPC <drivespublic@gmail.com>
2022-07-25 23:12:13 +0900

    chap 6 finish

---
+++ pages/api/client-test.tsx
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "../../libs/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await client.user.create({
+    data: {
+      email: "choiddh@test.com",
+      name: "hi",
+    },
+  });
+
+  res.status(201);
+
+  res.json({
+    ok: true,
+    data: "created",
+  });
+}
